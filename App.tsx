aaf251d0eda6a7cbd0da6825d0b4a09237450be6
Matheus Alves <matheus.adeoliveira11@gmail.com>
2022-11-06 19:40:01 -0300

    all application components

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { SignIn } from './src/screens/Signin';
+import { New } from './src/screens/New';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
@@ -18,7 +18,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <New /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
